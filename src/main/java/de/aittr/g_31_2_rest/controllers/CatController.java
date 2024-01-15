88e68c62207e2c54034a47a5f6c53d7df5798bbc
Egor Knestiapin <knesp@Yandex.ru>
2024-01-15 20:21:07 +0400

    initial commit

---
+++ src/main/java/de/aittr/g_31_2_rest/controllers/CatController.java
@@ -0,0 +1,32 @@
+package de.aittr.g_31_2_rest.controllers;
+
+import de.aittr.g_31_2_rest.domain.Cat;
+import de.aittr.g_31_2_rest.services.CatService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cats")
+public class CatController {
+    private CatService service;
+
+    public CatController(CatService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Cat> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cat getById(@PathVariable int id) {
+        return service.getById(id);
+    }
+
+    @GetMapping("/get")
+    public Cat getById2(@RequestParam int id) {
+        return service.getById(id);
+    }
+}
